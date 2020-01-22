643ba3834b8a5de3082698fce84224974ccc1d5f
Peter Scopes <peter.scopes@nccgroup.trust>
2020-01-22 15:58:59 +0000

    Bumps version for release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt', 'r') as fh:
     install_requirements = fh.read().split()
 setuptools.setup(
     name='reactor',
-    version='1.1.0',
+    version='1.2.0',
     author='Peter Scopes',
     author_email='peter.scopes@nccgroup.com',
     maintainer='Peter Scopes',
